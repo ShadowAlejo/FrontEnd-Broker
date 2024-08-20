7fb791ddeded4f2bd8fa96b8c1e47b583468a43c
unknown <alejot2003@gmail.com>
2024-08-20 08:41:21 -0500

    Front img 3

--- src/components/common/ProductSection.vue
+++ src/components/common/ProductSection.vue
@@ -22,37 +22,37 @@ import ProductCard from './ProductCard.vue'
 
 const products = [
   {
-    image: '/assets/respaldoFamiliar.svg',
+    image: '/src/assets/respaldoFamiliar.svg',
     title: 'Respaldo Familiar',
     description: 'Un seguro que te ayuda a planificar tu futuro en el caso de cualquier eventualidad.',
     routeName: 'Respaldo' // Nombre de la ruta correspondiente
   },
   {
-    image: '../src/assets/fianzas.svg',
+    image: '/src/assets/fianzas.svg',
     title: 'Fianzas',
     description: 'Te acompañamos en el cumplimiento de tus obligaciones con un servicio ágil y una asesoría especializada.',
     routeName: 'Fianzas' // Nombre de la ruta correspondiente
   },
   {
-    image: '../src/assets/planJubilacion.svg',
+    image: '/src/assets/planJubilacion.svg',
     title: 'Plan de jubilación',
     description: 'Solución para cumplir tu meta y llegar a la edad de retiro sin preocupaciones.',
     routeName: 'PlanJubilacion' // Nombre de la ruta correspondiente
   },
   {
-    image: '../src/assets/seguroSalud.svg',
+    image: '/src/assets/seguroSalud.svg',
     title: 'Seguro de Salud',
     description: 'Cubre tus necesidades médicas y las de tu familia con el mejor servicio de salud.',
     routeName: 'SeguroSalud' // Nombre de la ruta correspondiente
   },
   {
-    image: '../src/assets/seguroVehiculos.svg',
+    image: '/src/assets/seguroVehiculos.svg',
     title: 'Seguro de Vehículos',
     description: 'Protege tu vehículo contra accidentes y robos con nuestro seguro integral.',
     routeName: 'SeguroVehiculos' // Nombre de la ruta correspondiente
   },
   {
-    image: 'src/assets/seguroHogar.svg',
+    image: '/src/assets/seguroHogar.svg',
     title: 'Seguro de Hogar',
     description: 'Asegura tu hogar contra cualquier eventualidad con nuestras coberturas completas.',
     routeName: 'SeguroHogar' // Nombre de la ruta correspondiente
